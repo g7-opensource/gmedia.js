fb72a71b23279a659a72a7a3cc86335456a32a3f
yuanjianping <mouryuan@163.com>
2025-01-06 15:20:31 +0800

    更新测试环境配置

--- demo/network.js
+++ demo/network.js
@@ -3,14 +3,26 @@
 //仅测试环境生效
 //仅用于测试
 /**********************************/
-
+//114.116.210.142 成都IDC测试环境
 function getRealplayUrl(imei,channel,avitemType,streamType,callback)
 {
     let url = "http://114.116.210.142:9234/vega/video/open_live?imei=" +  imei +
               "&channels=" + channel +
               "&avitemType=" + avitemType +
               "&streamType=" + streamType +
-              "&hls=true&https=true";
+              "&hls=true&userSource=test&storage=true";
+    httpGet(url,callback);
+}
+
+function getGBRealplayUrl(ipcid,callback)
+{
+    let url = "http://172.22.33.225:8093/e6/api/realTime/start?ipcCode=" +  ipcid;
+    httpGet(url,callback);
+}
+
+function getGBTalkUrl(ipcid,callback)
+{
+    let url = "http://172.22.33.225:8093/e6/api/talk?ipcCode=" +  ipcid;
     httpGet(url,callback);
 }
 
@@ -18,7 +30,7 @@ function getTalkUrl(imei,channel,callback)
 {
     let url = "http://114.116.210.142:9234/vega/video/open_live?imei=" +  imei +
               "&channels=" + channel +
-              "&avitemType=" + 2 + "&https=true&storage=true";
+              "&avitemType=" + 2 + "&storage=true";
     httpGet(url,callback);
 }
 
@@ -28,7 +40,7 @@ function getPlaybackUrl(imei,channel,start,end,callback)
               "&channels=" + channel +
               "&from=" + start +
               "&to=" + end + 
-              "&avitemType=0&streamType=1&memType=1&https=true";
+              "&avitemType=0&streamType=1&memType=1&userSource=vccs";
     httpGet(url,callback);
 }
 
